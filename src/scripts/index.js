867a322005402231b8dcebbab2fdbae25bb7ff0e
Timur Khakkiev <khakkievtimur@yandex.ru>
2021-10-07 18:25:48 +0300

    Добавлена верстка, адаптив и анимация

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,19 @@
-console.log('hi');
+const woman = document.querySelector(".woman");
+const hole = document.querySelector(".hole");
+
+const womanAppear = () => {
+  setTimeout(() => {
+    hole.classList.add('active')
+  }, 500)
+
+
+  setTimeout(() => {
+    woman.classList.add('active')
+  }, 1000)
+
+  setTimeout(() => {
+    woman.classList.add('fly')
+  }, 2500)
+}
+
+womanAppear()
